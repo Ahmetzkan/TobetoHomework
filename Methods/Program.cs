d01cc67850f91c8552a751c2edbaad1614a8f2ad
Ahmet Özkan <ahmet.ozkan98@hotmail.com>
2023-10-24 00:39:44 +0300

    C# Dersleri 24: Challenge : Ref Keyword İle Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -2,7 +2,12 @@
 Add();
 
 var result = Add2(20);
-Console.WriteLine(result);
+int number1 = 20;
+int number2 = 100;
+var result2 = Add3(ref number1,number2); //referans tip ile Add3 methodundaki number1 değeri 30 olur.
+
+Console.WriteLine(result2);
+Console.WriteLine(number1);
 Console.ReadLine();
 
 
@@ -18,3 +23,9 @@ static int Add2(int number1,int number2=40)
 
 }
 
+static int Add3(ref int number1, int number2)
+{
+    number1 = 30;
+    return number1 + number2 ;
+
+}
