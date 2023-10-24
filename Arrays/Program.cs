84e83ef931c9ab63796e6f0ae4a88ad6a5f38c33
Ahmet Özkan <ahmet.ozkan98@hotmail.com>
2023-10-24 11:23:15 +0300

    C# Dersleri 29: Çok Boyutlu Diziler

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -13,3 +13,20 @@ foreach (var student in students2)
     Console.WriteLine(student);
 }
 
+string[,] regions = new string[5, 3]
+{
+    { "İstanbul","İzmit","Balıkesir" },
+    { "Ankara","Konya","Kırıkkale" },
+    { "Antalya","Adana","Mersin" },
+    { "Rize","Trabzon","Samsun" },
+    { "İzmir","Muğla","Manisa" },
+};
+
+for (int i = 0; i <= regions.GetUpperBound(0); i++) //GetUpperBound: Bir dizinin son indeks numarasını verir.
+{
+    for (int j = 0; j <= regions.GetUpperBound(1); j++)
+    {
+        Console.WriteLine(regions[i,j]);
+    }
+    Console.WriteLine("*********");
+}
