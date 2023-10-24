8eb1d89bd1b38a46f720b268ac92987468488100
Ahmet Özkan <ahmet.ozkan98@hotmail.com>
2023-10-24 11:16:07 +0300

    C# Dersleri 28: Diziler

---
+++ Arrays/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] students = new string[3];
+students[0] = "Engin";
+students[1] = "Ahmet";
+students[2] = "Derin";
+
+string[] students2 = { "Engin", "Ahmet", "Derin" };
+students[0] = "Engin";
+students[1] = "Ahmet";
+students[2] = "Derin";
+
+foreach (var student in students2)
+{
+    Console.WriteLine(student);
+}
+
