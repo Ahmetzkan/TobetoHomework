ab7cfca390e31a1f2a18e97ef3a58a964a30d0b7
Ahmet Özkan <ahmet.ozkan98@hotmail.com>
2023-10-26 22:47:55 +0300

    C# Dersleri 46: Kalıtıma Giriş

---
+++ Inheritance/Program.cs
@@ -0,0 +1,36 @@
+﻿Person[] persons = new Person[3]
+{
+    new Customer()
+    {
+        FirstName="Ahmet"
+    },
+    new Student() {
+        FirstName="Engin"
+    },
+    new Person() {
+        FirstName="Salih"
+    }
+};
+
+foreach (var person in persons)
+{
+    Console.WriteLine(person.FirstName);
+}
+//İnheritance'ın İnterfaceden farkı inheritance 2 defa miras alamaz ama interface alır.
+//İnterfaceler direkt interface ile tanımlanırken,inheritance direk "Customer:Person" gibi tanımlanır.
+
+class Person
+{ 
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+class Customer:Person
+{
+    public string City { get; set; }
+}
+class Student:Person
+{
+    public string Department { get; set; }
+}
