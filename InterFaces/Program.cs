dfcaaeb61f4b6126af2bc2463b8611825fc4a156
Ahmetzkan <ahmet.ozkan98@hotmail.com>
2023-10-26 19:15:11 +0300

    C# Dersleri 42: İnterfaceleri Neden Kullanıyoruz?

--- InterFaces/Program.cs
+++ InterFaces/Program.cs
@@ -1,9 +1,38 @@
-﻿interface IPerson
+﻿using System.Net;
+using static IPerson;
+
+PersonManager manager = new PersonManager();
+Customer customer = new Customer 
+{
+    Id=1,
+    FirstName="Ahmet",
+    LastName="Özkan",
+    Address="Ankara" 
+};
+Student student = new Student()
+{
+    Id = 2,
+    FirstName = "Derin",
+    LastName = "Demiroğ",
+    Departmant ="Computer Sciences"
+};
+Worker worker = new Worker()
+{
+    Id = 2,
+    FirstName = "Ali",
+    LastName = "Özkan",
+    Departmant ="Computer Sciences"
+};
+
+manager.Add(customer);
+manager.Add(student);
+manager.Add(worker);
+
+interface IPerson
 {
     int Id { get; set; }
     string FirstName { get; set; }
     string LastName { get; set; }
-
 
     class Customer : IPerson
     {
@@ -22,5 +51,24 @@
 
         public string Departmant { get; set; }
 
+    }
+
+    class Worker : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Departmant { get; set; }
+
+    }
+}
+
+class PersonManager
+{
+    public void Add(IPerson person) 
+    {
+        Console.WriteLine(person.FirstName);
+        Console.WriteLine(person.LastName);
     }
 }
